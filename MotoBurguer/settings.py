98be7b95b7f8fb7ae1ce836d514bd1720461b82e
gonmarmar5 <80246529+gonmarmar5@users.noreply.github.com>
2024-07-31 23:43:15 +0200

    feat: Added new menu

--- MotoBurguer/settings.py
+++ MotoBurguer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4&h2^+t+lpq+rqgmxb&^oekkowbnkn2u09n39gntd(9yy8en7k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.131', 'localhost', '127.0.0.1']
 
 # Application definition
 
